9e9cb24a9c05ef7a85cfa03d767759272617d0c3
GestorEterno <eternalgenesisgestor@gmail.com>
2025-05-23 18:38:07 -0300

    Delete components.js

--- components.js
+++
@@ -1,7 +0,0 @@
-export const routes = {
-  '#/inicio': { view: 'home.view.html', script: './home.js' },
-  '#/habits': { view: 'habits.view.html', script: './habits.js' },
-  '#/diary': { view: 'diary.view.html', script: './diary.js' },
-  '#/progress': { view: 'progress.view.html', script: './progress.js' },
-  '#/profile': { view: 'profile.view.html', script: './profile.js' },
-};
