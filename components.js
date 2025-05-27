66867f6ebb67c9c5fb6c96b5104e49e5c40a5207
GestorEterno <eternalgenesisgestor@gmail.com>
2025-05-26 21:16:09 -0300

    Add files via upload

---
+++ components.js
@@ -0,0 +1,29 @@
+// 🧱 BLOQUE 1 – components.js
+
+// Mapa de rutas para la SPA
+const routes = {
+  inicio: {
+    view: "home.view.html",
+    script: "home.js",
+  },
+  calendar: {
+    view: "calendar.view.html",
+    script: "calendar.js",
+  },
+  habits: {
+    view: "habits.view.html",
+    script: "habits.js",
+  },
+tasks: {
+  view: "tasks.view.html",
+  script: "tasks.js",
+},
+  progress: {
+    view: "progress.view.html",
+    script: "progress.js",
+  },
+  profile: {
+    view: "profile.view.html",
+    script: "profile.js",
+  },
+};
