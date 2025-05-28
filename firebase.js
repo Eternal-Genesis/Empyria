b31d41a78c0df7c71070217b712acc7f4dd02e5a
GestorEterno <eternalgenesisgestor@gmail.com>
2025-05-28 17:59:51 -0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,27 +1,23 @@
-// 🧱 BLOQUE – firebase.js (versión ES6 modular completa)
+// BLOQUE – firebase.js
 
-// Importar solo módulos ES
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-firestore.js";
 
-// Configuración de tu proyecto Firebase
 const firebaseConfig = {
-  apiKey: "AIzaSyB9M6vM_aD6k_7SWTdU9ItHqbJVPt1wFo4",
-  authDomain: "genesissystem-first.firebaseapp.com",
-  projectId: "genesissystem-first",
-  storageBucket: "genesissystem-first.appspot.com",
-  messagingSenderId: "90166657524",
-  appId: "1:90166657524:web:cb0d09adb367789717b9a1",
-  measurementId: "G-W6NEEBV2KE"
+  apiKey: "AIzaSyCZGoY5c9LeQQwPigOHR6LQDZRFNLMIzyU",
+  authDomain: "genesissystem-private.firebaseapp.com",
+  projectId: "genesissystem-private",
+  storageBucket: "genesissystem-private.firebasestorage.app",
+  messagingSenderId: "556103929119",
+  appId: "1:556103929119:web:dac21b7061cb9dfd05742a",
+  measurementId: "G-GQRRMN16HC"
 };
 
-// Inicializar Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getFirestore(app);
 
-console.log("🔥 Firebase inicializado (modular)");
+console.log("🔥 Firebase inicializado correctamente (PRIVATE)");
 
-// Exportar para uso en otros módulos
 export { app, db, auth };
